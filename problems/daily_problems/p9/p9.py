3210e5fd923c90729088341005136b15b94048be
Paul Kruse <NKPmedia@users.noreply.github.com>
2022-12-03 15:10:35 +0000

    TMP

---
+++ problems/daily_problems/p9/p9.py
@@ -0,0 +1,18 @@
+"""
+Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
+
+For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+
+Follow-up: Can you do this in O(N) time and constant space?
+"""
+from typing import Tuple
+import unittest
+
+def get_largest_non_adj_sum(numbers: List[int]) -> int:
+    
+
+
+class Test_P9(unittest.TestCase):
+
+    def test_cases(self):
+        assert get_largest_non_adj_sum()
